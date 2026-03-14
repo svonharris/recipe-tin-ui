59285269f8b3144184c7f4e945f352a6295790ab
Sheonna Harris <svonharris@gmail.com>
2026-03-14 01:31:23 -0400

    Created ButrientBar component, module.css, story.

---
+++ src/components/NutrientBar/NutrientBar.tsx
@@ -0,0 +1,49 @@
+import ProgressBar from "../ProgressBar/ProgressBar";
+import styles from "./NutrientBar.module.css";
+
+type NutrientBarProps = {
+  value: number;
+  macronutrient: string;
+};
+
+const COLORS: Record<string, { track: string; value: string }> = {
+  protein: {
+    track: "var(--color-secondary-100)",
+    value: "var(--color-secondary-500)",
+  },
+  carb: {
+    track: "var(--color-information-100)",
+    value: "var(--color-information-500)",
+  },
+  fat: {
+    track: "var(--color-warning-100)",
+    value: "var(--color-warning-500)",
+  },
+};
+
+const FALLBACK_COLORS = {
+  track: "var(--color-gray-100)",
+  value: "var(--color-gray-500)",
+};
+
+const NutrientBar = ({ value, macronutrient }: NutrientBarProps) => {
+  const colors = COLORS[macronutrient] ?? FALLBACK_COLORS;
+
+  return (
+    <div className={styles.nutrientBar}>
+      <ProgressBar
+        value={value}
+        trackColor={colors.track}
+        valueColor={colors.value}
+      />
+      <label className={styles.label}>
+        <p className={`text-lg-bold ${styles.amount}`}>{`${value}g`}</p>
+        <p className={`text-sm-regular ${styles.macronutrient}`}>
+          {macronutrient}
+        </p>
+      </label>
+    </div>
+  );
+};
+
+export default NutrientBar;
