5ec0310ea8d0b53a445722f94dd365723db1e407
Sheonna Harris <svonharris@gmail.com>
2026-03-15 22:38:00 -0400

    Created RecipeGallery to display search results independently.

---
+++ src/components/RecipeGallery/RecipeGallery.tsx
@@ -0,0 +1,36 @@
+import RecipeCard from "../RecipeCard/RecipeCard";
+
+type Recipe = {
+  recipe: {
+    label: string;
+    dishType: string[];
+    images: { REGULAR: { url: string } };
+    calories: number;
+    digest: { label: string; total: number; id: string }[];
+    ingredients: { text: string }[];
+    yield: number;
+  };
+};
+
+type RecipeGalleryProps = {
+  recipes: Recipe[];
+};
+
+const RecipeGallery = ({ recipes }: RecipeGalleryProps) => (
+  <div className="recipe-gallery">
+    {recipes.map((recipe, index) => (
+      <RecipeCard
+        key={index}
+        title={recipe.recipe.label}
+        type={recipe.recipe.dishType}
+        image={recipe.recipe.images.REGULAR.url}
+        calories={recipe.recipe.calories}
+        macros={recipe.recipe.digest}
+        ingredients={recipe.recipe.ingredients}
+        servings={recipe.recipe.yield}
+      />
+    ))}
+  </div>
+);
+
+export default RecipeGallery;
