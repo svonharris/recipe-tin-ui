f632ade4a5667d2343cf0f4652e45310d3ce2207
Sheonna Harris <svonharris@gmail.com>
2026-03-16 22:37:07 -0400

    Changed div to Stack component. Added Prop: totalTime. Updated commented notes.

--- src/components/RecipeGallery/RecipeGallery.tsx
+++ src/components/RecipeGallery/RecipeGallery.tsx
@@ -4,11 +4,13 @@
 // <RecipeGallery recipes={recipes} />
 
 import RecipeCard from "../RecipeCard/RecipeCard";
+import Stack from "../Stack/Stack";
 
 type Recipe = {
   recipe: {
     label: string;
     images: { REGULAR: { url: string } };
+    totalTime: number;
   };
 };
 
@@ -18,26 +20,21 @@ type RecipeGalleryProps = {
 
 const RecipeGallery = ({ recipes }: RecipeGalleryProps) => {
   return (
-    <div className="recipe-gallery">
+    <Stack gap={16}>
       {recipes.map((recipe, index) => (
         <RecipeCard
           key={index}
           title={recipe.recipe.label}
           image={recipe.recipe.images.REGULAR.url}
-
-          // Gallery Cards
-          // image={recipe.recipe.images.REGULAR.url}
-          // reviews={[]}
-          // cookTime={0}
-          // title={recipe.recipe.label}
+          cookTime={recipe.recipe.totalTime}
         />
       ))}
-    </div>
+    </Stack>
   );
 };
 
 // Create a link to the recipe details page when a recipe card is clicked. This will require setting up routing in the app and passing the selected recipe's data to the details page. Sample data to be passed to the details page can include the
-// recipe's title, dishType, image, calories, macros, ingredients, servings, reviews, cooktime, description, and instructions. The details page can then use this data to display a more comprehensive view of the recipe, including the instructions and any additional information.
+// recipe's title, dishType, image { LARGE?: { url: string }; REGULAR: { url: string } }, calories, macros, ingredients {recipe.recipe.ingredients[i].food/measure/quantity/image}, servings, cooktime, description {recipe.recipe.url}(send user to recipe source), and instructions. The details page can then use this data to display a more comprehensive view of the recipe, including the instructions and any additional information.
 // Macros sent to NutrientBar for display, and Ingredients sent to IngredientsItem.
 
 export default RecipeGallery;
