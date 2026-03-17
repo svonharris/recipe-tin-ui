f369502886f3cdab013fce126bd43e0854fff8e2
Sheonna Harris <svonharris@gmail.com>
2026-03-16 22:40:16 -0400

    Removed ClockCircle icon. Removed ReadMore component. Added RecipeDuration component. Changed where story folder lives. Update image path. Removed headings: Description. Removed calories html.

--- src/components/Page/Page.stories.tsx
+++ src/components/Page/Page.stories.tsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
 import type { Meta, StoryObj } from "@storybook/react";
-import { ClockCircle, MenuLineHorizontal01, Filter01 } from "../../icons";
+import { MenuLineHorizontal01, Filter01 } from "../../icons";
 import Page from "./Page";
 import Image from "../Image/Image";
 import Ratings from "../Ratings/Ratings";
 import NutrientBar from "../NutrientBar/NutrientBar";
 import IngredientsItem from "../IngredientsItem/IngredientsItem";
-import ReadMore from "../ReadMore/ReadMore";
 import styles from "./Page.module.css";
 import Heading from "../Heading/Heading";
 import Inline from "../Inline/Inline";
@@ -14,9 +13,10 @@ import Stack from "../Stack/Stack";
 import ButtonIcon from "../ButtonIcon/ButtonIcon";
 import FormSearch from "../FormSearch/FormSeach";
 import RecipeGallery from "../RecipeGallery/RecipeGallery";
+import RecipeDuration from "../RecipeDuration/RecipeDuration";
 
 const meta = {
-  title: "Components/Page",
+  title: "Internal/Page",
   component: Page,
 } satisfies Meta<typeof Page>;
 
@@ -76,16 +76,13 @@ export const RecipeDetails: Story = {
     children: (
       <div style={{ maxWidth: "375px" }}>
         <Image
-          src="../Bruschetta-Ricotta-Pesto.jpg"
+          src="../bruschetta.jpeg"
           altText="Bruschetta with ricotta and pesto"
           borderRadius={"var(--image-border-bottom)"}
         />
         <Inline gap={0} justify="between">
           <Ratings rating={3} reviews={300} />
-          <div className={`${styles.cookTime}`}>
-            <ClockCircle size={16} color="var(--color-gray-800)" />
-            <p className={`text-xs-regular ${styles.cookTimeText}`}>20 min</p>
-          </div>
+          <RecipeDuration duration={45} />
         </Inline>
         <Heading
           htmlElement="h1"
@@ -96,42 +93,10 @@ export const RecipeDetails: Story = {
         </Heading>
         <hr className={styles.divider} />
 
-        <Stack>
-          <Heading
-            htmlElement="h2"
-            textClassName="text-lg-bold"
-            color="var(--color-dark-500)"
-          >
-            Description
-          </Heading>
-          <ReadMore
-            className={`text-md-regular ${styles.recipeDescriptionText}`}
-            text="This bruschetta recipe is a delicious and easy appetizer that combines the flavors of fresh tomatoes, creamy ricotta cheese, and fragrant basil pesto. Perfect for entertaining or as a light snack, this dish is sure to impress your guests with its vibrant colors and bold flavors. Toast slices of crusty bread until golden, spread with creamy ricotta, top with a fresh tomato mixture, and finish with a drizzle of homemade basil pesto for a stunning and satisfying dish."
-          />
-        </Stack>
         <Inline gap={24}>
           <NutrientBar value={16} macronutrient={"protein"} />
           <NutrientBar value={65} macronutrient={"carb"} />
           <NutrientBar value={90} macronutrient={"fat"} />
-          <div>
-            <progress
-              id="calories"
-              value={1134}
-              max={2000}
-              style={
-                {
-                  "--p1": `${((64 / 1134) * 100).toFixed(1)}%`,
-                  "--p2": `${(((64 + 260) / 1134) * 100).toFixed(1)}%`,
-                } as React.CSSProperties
-              }
-            />
-            <label htmlFor="calories">
-              <p className={`text-lg-bold ${styles.nutritionAmount}`}>1134</p>
-              <p className={`text-sm-regular ${styles.nutritionLabel}`}>
-                calories
-              </p>
-            </label>
-          </div>
         </Inline>
         <Stack>
           <Heading
@@ -153,7 +118,8 @@ export const RecipeDetails: Story = {
             Instructions
           </Heading>
           <p className={`text-md-regular ${styles.instructionsContent}`}>
-            Follow these steps to prepare the dish.
+            Follow the url for detailed steps to prepare the dish.
+            <a>Click Here</a>
           </p>
         </Stack>
       </div>
