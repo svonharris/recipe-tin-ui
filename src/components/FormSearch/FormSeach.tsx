f143990da1089892a7d522464aab4c42c8293d33
Sheonna Harris <svonharris@gmail.com>
2026-03-17 00:18:41 -0400

    Added functionality for Recommeneded feature. Form uses arrays [foodCategories, meal] to do a random query of recipes. Added prop to hide search form when not in use.

--- src/components/FormSearch/FormSeach.tsx
+++ src/components/FormSearch/FormSeach.tsx
@@ -9,25 +9,45 @@ import { Search01 } from "../../icons";
 
 type FormSearchProps = {
   onResults: (recipes: any[]) => void;
+  recommend?: boolean;
+  hide?: boolean;
 };
 
-const FormSearch = ({ onResults }: FormSearchProps) => {
+const FormSearch = ({
+  onResults,
+  recommend = false,
+  hide = false,
+}: FormSearchProps) => {
   const ID = import.meta.env.VITE_ID;
   const KEY = import.meta.env.VITE_KEY;
 
+  const foodCategories = ["chicken", "pasta", "salad", "beef", "fish", "soup"];
+  const meal = ["breakfast", "brunch", "lunch/dinner", "snack", "teatime"];
+
   const [search, setSearch] = useState("");
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState(
+    recommend
+      ? foodCategories[Math.floor(Math.random() * foodCategories.length)]
+      : "",
+  );
 
   useEffect(() => {
     getRecipes();
   }, [query]);
 
   const getRecipes = async () => {
+    if (!recommend && !query) return; // prevents empty search on mount
+
+    const mealType = recommend
+      ? meal[Math.floor(Math.random() * meal.length)]
+      : undefined;
+    const mealTypeParam = mealType ? `&mealType=${mealType}` : "";
+
     const response = await fetch(
-      `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${ID}&app_key=${KEY}`,
+      `https://api.edamam.com/api/recipes/v2?type=public&q=${query}${mealTypeParam}&app_id=${ID}&app_key=${KEY}`,
     );
     const data = await response.json();
-    // console.log("FormSearch results:", data.hits);
+    console.log("FormSearch results:", data.hits);
     onResults(data.hits);
   };
 
@@ -38,7 +58,7 @@ const FormSearch = ({ onResults }: FormSearchProps) => {
   };
 
   return (
-    <form onSubmit={getSearch} style={{ width: "100%" }}>
+    <form onSubmit={getSearch} style={{ width: "100%" }} hidden={hide}>
       <FormInput
         label="Search"
         id="search"
