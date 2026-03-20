f9c1b85ac3b1e591185ea77f47dac7b0d355d889
Sheonna Harris <svonharris@gmail.com>
2026-03-20 03:28:27 -0400

    Icon now accepts children. Icon has two more variants: secondary, tertiary. Icon now as a "xl" size.

--- src/components/ButtonIcon/ButtonIcon.tsx
+++ src/components/ButtonIcon/ButtonIcon.tsx
@@ -1,26 +1,38 @@
 import React from "react";
 import style from "./ButtonIcon.module.css";
 import "../../styles/buttons.css";
+import Stack from "../Stack/Stack";
 
 type ButtonIconProps = {
+  children?: React.ReactNode;
   icon: React.ReactElement<{ size?: number; color?: string }>;
   onClick?: () => void;
   title?: string;
   type?: "button" | "submit";
   disabled?: boolean;
   rounded?: boolean;
-  size?: "lg" | "md" | "sm";
-  variant?: "primary" | "primaryGhost";
+
+  size?: "xl" | "lg" | "md" | "sm";
+  variant?: "primary" | "secondary" | "tertiary" | "primaryGhost";
 };
 
-const SIZE: Record<"lg" | "md" | "sm", { className: string; px: number }> = {
+const SIZE: Record<
+  "xl" | "lg" | "md" | "sm",
+  { className: string; px: number }
+> = {
+  xl: { className: `${style.icon} ${style.iconXl}`, px: 48 },
   lg: { className: `${style.icon} ${style.iconLg}`, px: 28 },
   md: { className: `${style.icon} ${style.iconMd}`, px: 24 },
   sm: { className: `${style.icon} ${style.iconSm}`, px: 20 },
 };
 
-const VARIANT_STYLES: Record<"primary" | "primaryGhost", string> = {
+const VARIANT_STYLES: Record<
+  "primary" | "secondary" | "tertiary" | "primaryGhost",
+  string
+> = {
   primary: "primaryButton",
+  secondary: "secondaryButton",
+  tertiary: "tertiaryButton",
   primaryGhost: "primaryGhost",
 };
 
@@ -30,6 +42,7 @@ const DISABLED_STYLES: Record<"outline" | "filled", string> = {
 };
 
 const ButtonIcon = ({
+  children,
   icon,
   onClick,
   title,
@@ -42,24 +55,27 @@ const ButtonIcon = ({
   const isFilled = variant !== "primaryGhost"; // or a Set if variants grow
 
   return (
-    <button
-      className={`buttonBase ${SIZE[size].className}  
+    <Stack gap={8} align="center">
+      <button
+        className={`buttonBase ${SIZE[size].className}  
       ${rounded ? style.rounded : ""} 
       ${
         disabled
           ? DISABLED_STYLES[isFilled ? "filled" : "outline"]
           : VARIANT_STYLES[variant]
       }`}
-      onClick={onClick}
-      type={type}
-      disabled={disabled}
-      title={title}
-    >
-      {React.cloneElement(icon, {
-        size: SIZE[size].px,
-        color: "inherit",
-      })}
-    </button>
+        onClick={onClick}
+        type={type}
+        disabled={disabled}
+        title={title}
+      >
+        {React.cloneElement(icon, {
+          size: SIZE[size].px,
+          color: "inherit",
+        })}
+      </button>
+      {children && <p className="text-md-regular">{children}</p>}
+    </Stack>
   );
 };
 
