7e467c9fdd82c687e0d5f4017a8b48ef5adba09d
Sheonna Harris <svonharris@gmail.com>
2026-03-19 23:23:57 -0400

    Created a NavBar component, css, and story.

---
+++ src/components/NavBar/NavBar.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import NavBar from "../NavBar/NavBar";
+import { Home04, Compass, Search01 } from "../../icons";
+
+const meta = {
+  title: "Components/Containers/NavBar",
+  component: NavBar,
+  argTypes: {
+    navItems: { control: false },
+  },
+} satisfies Meta<typeof NavBar>;
+
+export default meta;
+type Story = StoryObj<typeof NavBar>;
+
+export const Default: Story = {
+  args: {
+    navItems: [
+      { name: "Home", icon: <Home04 />, href: "/" },
+      { name: "Discover", icon: <Compass />, href: "/discover" },
+      { name: "Search", icon: <Search01 />, href: "/search" },
+    ],
+  },
+};
---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import style from "./NavBar.module.css";
+
+type NavItem = {
+  name: string;
+  icon: React.ReactElement<{ size?: number; color?: string }>;
+  href: string;
+};
+
+type NavBarProps = {
+  navItems: NavItem[];
+};
+
+const NavBar = ({ navItems }: NavBarProps) => {
+  return (
+    <nav aria-label="Main Navigation">
+      <ul className={style.navList}>
+        {navItems.map((item, index) => (
+          <li key={index}>
+            <a href={item.href} className={style.navLink}>
+              {React.cloneElement(item.icon, {
+                size: 28,
+                color: "inherit",
+              })}
+              <p className="text-md-regular">{item.name}</p>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
