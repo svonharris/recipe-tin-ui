3a442a0457a96008b9a887db4c02cdb3bdfc1ec6
Sheonna Harris <svonharris@gmail.com>
2026-03-25 23:06:17 -0400

    Corrected image path.

--- src/mocks/mockRecipes.ts
+++ src/mocks/mockRecipes.ts
@@ -2,28 +2,28 @@ export const pastaRecipes = [
   {
     recipe: {
       label: "Pasta Primavera",
-      images: { REGULAR: { url: "../pasta-primavera.jpg" } },
+      images: { REGULAR: { url: "./pasta-primavera.jpg" } },
       totalTime: 90,
     },
   },
   {
     recipe: {
       label: "Pasta alla Gricia",
-      images: { REGULAR: { url: "../Pasta-alla-Gricia.jpg" } },
+      images: { REGULAR: { url: "./Pasta-alla-Gricia.jpg" } },
       totalTime: 10,
     },
   },
   {
     recipe: {
       label: "Pasta e Fagioli Pasta",
-      images: { REGULAR: { url: "../Pasta-e-Fagioli-Pasta.jpg" } },
+      images: { REGULAR: { url: "./Pasta-e-Fagioli-Pasta.jpg" } },
       totalTime: 75,
     },
   },
   {
     recipe: {
       label: "Eggplant Rollitini Pasta",
-      images: { REGULAR: { url: "../Eggplant-Rollitini-Pasta.jpg" } },
+      images: { REGULAR: { url: "./Eggplant-Rollitini-Pasta.jpg" } },
       totalTime: 20,
     },
   },
