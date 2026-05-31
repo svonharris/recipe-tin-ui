86d8756a4e0e5a3bd3c1e44d9eae4a7fea996ca9
Sheonna Harris <svonharris@gmail.com>
2026-05-30 22:49:03 -0400

    chore: add post-commit hook to auto-tag releases on version change
    Adds a post-commit hook that compares the current and previous
    package.json versions and creates a git tag (vX.Y.Z) when a version
    bump is detected. Updates install-hooks.sh to install the new hook.

--- scripts/install-hooks.sh
+++ scripts/install-hooks.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 cp scripts/hooks/pre-commit .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
+cp scripts/hooks/post-commit .git/hooks/post-commit
+chmod +x .git/hooks/post-commit
 echo "Git hooks installed."
